d9845c267cba84d16097586401ba215ad0b3cbf8
Anjan Koradia <anjan.k@simformsolutions.com>
2023-05-26 19:55:22 +0530

    List all products to frontend

--- costumestore/website/urls.py
+++ costumestore/website/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page, name="home_page"),
-    path("cart", views.cart_page, name="cart_page"),
-    path("shop", views.shop_page, name="shop_page"),
-    path("contact", views.contact_page, name="contact_page"),
+    path("cart/", views.cart_page, name="cart_page"),
+    path("shop/<str:category>/", views.Shop_Page.as_view(), name="shop_page"),
+    path("contact/", views.contact_page, name="contact_page"),
 ]
--- costumestore/website/views.py
+++ costumestore/website/views.py
@@ -1,17 +1,33 @@
+from typing import Any
+from django.views.generic import ListView
 from django.shortcuts import render
-from django.contrib.auth import logout
+from vendor.models import Product
 
+def fetch_products(category):
+    if category == 'all':
+        return Product.objects.all()
+    
+    return Product.objects.filter(category=category)
 
 def home_page(req):
-    return render(req, "website/index.html")
+    products = Product.objects.all()[0:10]
+    return render(req, "website/index.html", {"products": products})
 
 
 def cart_page(req):
     return render(req, "website/cart.html")
 
-
-def shop_page(req):
-    return render(req, "website/shop.html")
+class Shop_Page(ListView):
+    model = Product
+    template_name = "website/shop.html"
+    context_object_name = 'products'
+    paginate_by = 18
+    
+    
+    def get_queryset(self):
+        category = self.kwargs["category"]
+        products = fetch_products(category)
+        return products
 
 
 def contact_page(req):
