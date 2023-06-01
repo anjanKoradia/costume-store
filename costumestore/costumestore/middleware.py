cb916fc378d827753832989ea019c24b4a50930d
Anjan Koradia <anjan.k@simformsolutions.com>
2023-06-01 19:39:43 +0530

    Resolve bug

--- costumestore/costumestore/middleware.py
+++ costumestore/costumestore/middleware.py
@@ -15,7 +15,6 @@ class PermissionsMiddleware:
             return response
 
         return redirect("home_page")
-        
 
     def check_visitor_permissions(self, request):
         if request.path.find("visitor") == 1 or request.path == "/":
@@ -24,19 +23,22 @@ class PermissionsMiddleware:
         return False
 
     def check_user_permissions(self, request):
+        if(request.user.role == "admin" and request.path.count("admin") != 0):
+            return True
+        
         if (request.user.role == "vendor" or request.user.role == "admin") and (
-            request.path.find("customer") == 1
+            request.path.count("customer") != 0
         ):
             return False
 
         if (
             request.user.role == "customer" or request.user.role == "admin"
-        ) and request.path.find("vendor") == 1:
+        ) and request.path.count("vendor") != 0:
             return False
 
         if (
             request.user.role == "customer" or request.user.role == "vendor"
-        ) and request.path.find("admin") == 1:
+        ) and request.path.count("admin") != 0:
             return False
 
         return True
